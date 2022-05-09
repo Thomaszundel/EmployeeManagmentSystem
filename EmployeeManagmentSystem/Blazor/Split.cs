d8cb3332d548984ec4681820adffc05f57fc568a
thomas zundel <thomas.zundel@student.centralia.edu>
2022-05-08 19:58:02 -0700

    added Blazor

---
+++ EmployeeManagmentSystem/Blazor/Split.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagmentSystem.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagmentSystem.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public EmpManagerDbContext? Context { get; set; }
+
+        public IEnumerable<string> Names => Context?.People.Select(p => p.FirstName) ?? Enumerable.Empty<string>();
+    }
+}
