436ce581cca1249a24f5e61ef5ef2c1e3ad18bf1
thomas zundel <thomas.zundel@student.centralia.edu>
2022-05-25 21:48:44 -0700

    Added Forms

--- EmployeeManagmentSystem/Models/Person.cs
+++ EmployeeManagmentSystem/Models/Person.cs
@@ -1,11 +1,22 @@
-﻿namespace EmployeeManagmentSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagmentSystem.Models
 {
     public class Person
     {
         public long PersonId { get; set; }
+        [Required(ErrorMessage = "A firstname is required")]
+        [MinLength(3, ErrorMessage ="Firstnames must be 3 or more characters")]
         public string FirstName { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "A lastname is required")]
+        [MinLength(3, ErrorMessage = "Lastnames must be 3 or more characters")]
         public string LastName { get; set; } = String.Empty;
+
+        [Range (1,long.MaxValue, ErrorMessage ="A department must be selected")]
         public long DepartmentId { get; set; }
+
+        [Range(1, long.MaxValue, ErrorMessage = "A location must be selected")]
         public long LocationId { get; set; }
 
         public Department? Department { get; set; }
