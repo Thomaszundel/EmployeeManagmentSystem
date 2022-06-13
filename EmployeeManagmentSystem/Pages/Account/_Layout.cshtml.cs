b0c6c81a80ec5b5df187bb42acacbd9688fdf2b0
thomas zundel <thomas.zundel@student.centralia.edu>
2022-06-12 23:05:17 -0700

    final changes

--- EmployeeManagmentSystem/Pages/Account/_Layout.cshtml.cs
+++ EmployeeManagmentSystem/Pages/Account/_Layout.cshtml.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace EmployeeManagmentSystem.Pages.Account
 {
     public class _LayoutModel : PageModel
     {
         public void OnGet()
         {
+
         }
     }
 }
